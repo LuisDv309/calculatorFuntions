5a043a5208a82d23e5178070a318b5581c71a418
Luis Perez Pena <270161235@yoobeestudent.ac.nz>
2021-10-05 12:16:32 +1300

    adding content

--- calculationFunction/calculationFunction.cpp
+++ calculationFunction/calculationFunction.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 using namespace std;
 //Function prototype//
+void line();
 void addNumbers();
 void substractNumbers();
 void multiplyNumbers();
@@ -17,9 +18,13 @@ int main()
 	multiplyNumbers();
 	divideNumbers();
 }
+void line() {
+	cout << "\n******************\n";
+}
 
 void addNumbers() {
 	float number1, number2, result;
+	cout << "\nadding numbers";
 	cout << "\nEnter number1" << endl;
 	cin >> number1;
 	cout << "\nEnter number2" << endl;
@@ -28,13 +33,44 @@ void addNumbers() {
 }
 	
 	void substractNumbers() {
+		float number1, number2, result;
+		cout << "\nsubstraction";
+		line();
 		cout << "\nEnter number1" << endl;
 		cin >> number1;
 		cout << "\nEnter number2" << endl;
 		cin >> number2;
 		result = number1 - number2;
+		cout << "\nThe difference of the two given numbers is:" << result << endl << endl;
 	}
 
+	void multiplyNumbers() {
+		float number1, number2, result;
+		cout << "\nMultiplication";
+		line();
+		cout << "\nEnter number1: ";
+		cin >> number1;
+		cout << "\nEnter number2: ";
+		cin >> number2;
+		result = number1 * number2;
+		cout << "\nThe product of the two given numbers is: " << result << endl << endl;
+	}
+
+	void divideNumbers() {
+		float number1, number2, result;
+		cout << "\nDivision";
+		line();
+		cout << "\nEnter number1: ";
+		cin >> number1;
+		cout << "\nEnter number2: ";
+		cin >> number2;
+		if (number2 != 0) {
+			result = number1 / number2;
+			cout << "\nThe difference of the two given numbers is: " << result << endl;
+		}
+		else {
+			cout << "\nSorry the number2 is equal to zero, so division overflow, try a different number." << endl << endl;
+		}
 
 
-
+	}
